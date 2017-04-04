5c057d22b490f33c67ed80aa8f6f34a52d780309
balpreetpunia <mebalpreetpunia@gmail.com>
2017-04-03 20:18:30 -0400

    Completed Start Form

---
+++ Assignment4/StartForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Assignment4
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.NewOrderButton = new System.Windows.Forms.Button();
+            this.SavedOrderButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(47, 40);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(255, 24);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Order Your Computer Today!";
+            // 
+            // NewOrderButton
+            // 
+            this.NewOrderButton.Location = new System.Drawing.Point(124, 236);
+            this.NewOrderButton.Name = "NewOrderButton";
+            this.NewOrderButton.Size = new System.Drawing.Size(116, 23);
+            this.NewOrderButton.TabIndex = 1;
+            this.NewOrderButton.Text = "Start A New Order";
+            this.NewOrderButton.UseVisualStyleBackColor = true;
+            // 
+            // SavedOrderButton
+            // 
+            this.SavedOrderButton.Location = new System.Drawing.Point(124, 278);
+            this.SavedOrderButton.Name = "SavedOrderButton";
+            this.SavedOrderButton.Size = new System.Drawing.Size(116, 23);
+            this.SavedOrderButton.TabIndex = 2;
+            this.SavedOrderButton.Text = "Open A Saved Order";
+            this.SavedOrderButton.UseVisualStyleBackColor = true;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(124, 317);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(116, 23);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 375);
+            this.ControlBox = false;
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.SavedOrderButton);
+            this.Controls.Add(this.NewOrderButton);
+            this.Controls.Add(this.TitleLabel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "StartForm";
+            this.Text = "Welcome to Dollar Computers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Button NewOrderButton;
+        private System.Windows.Forms.Button SavedOrderButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+

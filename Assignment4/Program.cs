e9cd221e64fb10230dc16fb880ea57e0716ea407
balpreetpunia <mebalpreetpunia@gmail.com>
2017-04-03 23:33:43 -0400

    Added splash screen

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -20,7 +20,7 @@ namespace Assignment4
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new StartForm());
+            Application.Run(new SplashScreen());
         }
     }
 }
